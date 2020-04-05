e999291a9968bd9dee5c22985b4e1db85cf2f940
simonojok19 <simonojok19@gmail.com>
2020-04-05 21:01:22 +0300

    completed the class

--- lib/classes/FormatDates.dart
+++ lib/classes/FormatDates.dart
@@ -4,4 +4,12 @@ class FormatDates {
   String dateFormatShortMonthDayYear(String date) {
     return DateFormat.yMMMd().format(DateTime.parse(date));
   }
+
+  String dateFormatShortDayNumber(String date) {
+    return DateFormat.d().format(DateTime.parse(date));
+  }
+
+  String dateFormatShortDayName(String date) {
+    return DateFormat.E().format(DateTime.parse(date));
+  }
 }
