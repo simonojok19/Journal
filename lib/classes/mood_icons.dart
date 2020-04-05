c99515321e2ad5c64f40de8f7fd806564326cb4d
simonojok19 <simonojok19@gmail.com>
2020-04-05 21:08:08 +0300

    added the icons

--- lib/classes/mood_icons.dart
+++ lib/classes/mood_icons.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class MoodIcons {
@@ -5,6 +6,13 @@ class MoodIcons {
   final Color color;
   final double rotation;
   final IconData icon;
+  const List<MoodIcons> _moodIconsList = const <MoodIcons>[
+    const MoodIcons(title: 'Very Satisfied', color: Colors.amber, rotation: 0.4, icon: Icons.sentiment_very_satisfied),
+    const MoodIcons(title: 'Satisfied', color: Colors.green, rotation: 0.2, icon: Icons.sentiment_satisfied),
+    const MoodIcons(title: 'Neutral', color: Colors.grey, rotation: 0.0, icon: Icons.sentiment_neutral),
+    const MoodIcons(title: 'Dissatisfied', color: Colors.cyan, rotation: -0.2, icon:Icons.sentiment_dissatisfied),
+    const MoodIcons(title: 'Very Dissatisfied', color: Colors.red, rotation: -0.4, icon: Icons.sentiment_very_dissatisfied),
+  ];
 
   const MoodIcons({ this.title, this.color, this.rotation, this.icon });
 }
