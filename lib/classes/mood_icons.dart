68af735af2f0619b2fb7ba578fa367a85072b632
simonojok19 <simonojok19@gmail.com>
2020-04-05 21:04:57 +0300

    added the constructor

---
+++ lib/classes/mood_icons.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+
+class MoodIcons {
+  final String title;
+  final Color color;
+  final double rotation;
+  final IconData icon;
+
+  const MoodIcons({ this.title, this.color, this.rotation, this.icon });
+}
