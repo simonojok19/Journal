1a6cdfde90e79532076c808ef305f4b3a22dc676
simonojok19 <simonojok19@gmail.com>
2020-04-05 19:42:56 +0300

    structured the file

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:journal/pages/home.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Journal',
+      theme: ThemeData(
+        primarySwatch: Colors.lightBlue,
+        canvasColor: Colors.lightGreen.shade50,
+        bottomAppBarColor: Colors.lightGreen
+      ),
+      home: Home(),
     );
   }
 }
